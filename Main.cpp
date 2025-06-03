1cc21305c1c44a09d0e38c98b2b4593035c9cd6a
Владислав <1132242462@pfur.ru>
2025-06-03 12:34:17 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,114 +1,251 @@
 #include <iostream>
-#include <string>
+#include <cmath>
 
-using namespace std;
+const double PI = acos(-1.0);
 
-// Структура элемента списка
-struct Node {
-    string name;
-    int age;
-    double score;
-    Node* next;
-
-    Node(string n, int a, double s) : name(n), age(a), score(s), next(nullptr) {}
+class Point {
+public:  
+    virtual double getX() const = 0;
+    virtual double getY() const = 0;
+    virtual void move(double dx, double dy) = 0;
+    virtual ~Point() {}
 };
 
-// Класс односвязного списка
-class LinkedList {
+class BasicPoint : public Point {
 private:
-    Node* head;
-
+    double x, y;
 public:
-    LinkedList() : head(nullptr) {}
+    BasicPoint(double x = 0.0, double y = 0.0) : x(x), y(y) {}
 
-    // Добавление элемента в начало списка
-    void addToFront(string name, int age, double score) {
-        Node* newNode = new Node(name, age, score);
-        newNode->next = head;
-        head = newNode;
+    double getX() const override { return x; }
+    double getY() const override { return y; }
+
+    void move(double dx, double dy) override {
+        x += dx;
+        y += dy;
     }
 
-    // Добавление элемента в конец списка
-    void addToEnd(string name, int age, double score) {
-        Node* newNode = new Node(name, age, score);
-        if (!head) {
-            head = newNode;
-            return;
-        }
-        Node* temp = head;
-        while (temp->next) temp = temp->next;
-        temp->next = newNode;
+    void setPosition(double newX, double newY) { x = newX; y = newY; }
+};
+
+class Shape {
+protected:
+    static void rotatePoint(double& x, double& y, double centerX, double centerY, double radians) {
+        double dx = x - centerX;
+        double dy = y - centerY;
+        x = centerX + dx * cos(radians) - dy * sin(radians);
+        y = centerY + dx * sin(radians) + dy * cos(radians);
+    }
+    
+public:
+    virtual void draw() const = 0;
+    virtual void erase() const = 0;
+    virtual void move(double dx, double dy) = 0;
+    virtual void rotate(double angle) = 0;
+    virtual ~Shape() {}
+};
+
+class Line : public Shape {
+private:
+    BasicPoint start, end;
+public:
+    Line(double x1, double y1, double x2, double y2)
+        : start(x1, y1), end(x2, y2) {}
+
+    void draw() const override {
+        std::cout << "Рисуется линия от точки (" << start.getX() << ", " << start.getY()
+                  << ") до точки (" << end.getX() << ", " << end.getY() << ")" << std::endl;
     }
 
-    // Добавление элемента после указанного имени
-    void addAfter(string target, string name, int age, double score) {
-        Node* temp = head;
-        while (temp && temp->name != target) temp = temp->next;
-        if (temp) {
-            Node* newNode = new Node(name, age, score);
-            newNode->next = temp->next;
-            temp->next = newNode;
-        }
+    void erase() const override {
+        std::cout << "Стирается линия" << std::endl;
     }
 
-    // Добавление элемента перед указанным именем
-    void addBefore(string target, string name, int age, double score) {
-        if (!head) return;
-        if (head->name == target) {
-            addToFront(name, age, score);
-            return;
-        }
-        Node* temp = head;
-        while (temp->next && temp->next->name != target) temp = temp->next;
-        if (temp->next) {
-            Node* newNode = new Node(name, age, score);
-            newNode->next = temp->next;
-            temp->next = newNode;
-        }
+    void move(double dx, double dy) override {
+        start.move(dx, dy);
+        end.move(dx, dy);
     }
 
-    // Удаление элемента по имени
-    void removeByName(string name) {
-        if (!head) return;
-        if (head->name == name) {
-            Node* temp = head;
-            head = head->next;
-            delete temp;
-            return;
-        }
-        Node* temp = head;
-        while (temp->next && temp->next->name != name) temp = temp->next;
-        if (temp->next) {
-            Node* toDelete = temp->next;
-            temp->next = temp->next->next;
-            delete toDelete;
-        }
+    void rotate(double angle) override {
+        double radians = angle * PI / 180.0;
+        double centerX = (start.getX() + end.getX()) / 2.0;
+        double centerY = (start.getY() + end.getY()) / 2.0;
+        
+        double x1 = start.getX();
+        double y1 = start.getY();
+        double x2 = end.getX();
+        double y2 = end.getY();
+        
+        rotatePoint(x1, y1, centerX, centerY, radians);
+        rotatePoint(x2, y2, centerX, centerY, radians);
+        
+        start.setPosition(x1, y1);
+        end.setPosition(x2, y2);
+    }
+};
+
+class Rectangle : public Shape {
+protected:
+    BasicPoint topLeft;
+    double width, height;
+public:
+    Rectangle(double x, double y, double w, double h)
+        : topLeft(x, y), width(w), height(h) {}
+
+    void draw() const override {
+        std::cout << "Рисуется прямоугольник с левой верхней точкой в (" 
+                  << topLeft.getX() << ", " << topLeft.getY()
+                  << "), ширина: " << width << ", высота: " << height << std::endl;
     }
 
-    // Вывод списка
-    void printList() {
-        Node* temp = head;
-        while (temp) {
-            cout << "Имя: " << temp->name << ", Возраст: " << temp->age << ", Оценка: " << temp->score << endl;
-            temp = temp->next;
-        }
+    void erase() const override {
+        std::cout << "Стирается прямоугольник" << std::endl;
     }
 
-// Основная функция
-int main() {
-    LinkedList list;
+    void move(double dx, double dy) override {
+        topLeft.move(dx, dy);
+    }
 
-    list.addToFront("Алиса", 20, 85.5);
-    list.addToEnd("Борис", 22, 90.2);
-    list.addAfter("Алиса", "Виктор", 21, 88.0);
-    list.addBefore("Борис", "Галина", 23, 92.1);
+    void rotate(double angle) override {
+        double radians = angle * PI / 180.0;
+        double centerX = topLeft.getX() + width / 2.0;
+        double centerY = topLeft.getY() + height / 2.0;
+        
+        double x1 = topLeft.getX();
+        double y1 = topLeft.getY();
+        double x2 = x1 + width;
+        double y2 = y1;
+        double x3 = x2;
+        double y3 = y1 + height;
+        double x4 = x1;
+        double y4 = y3;
 
-    cout << "Список после добавлений:" << endl;
-    list.printList();
+        rotatePoint(x1, y1, centerX, centerY, radians);
+        rotatePoint(x2, y2, centerX, centerY, radians);
+        rotatePoint(x3, y3, centerX, centerY, radians);
+        rotatePoint(x4, y4, centerX, centerY, radians);
 
-    list.removeByName("Виктор");
-    cout << "\nСписок после удаления Виктора:" << endl;
-    list.printList();
-
-    return 0;
-}
+        std::cout << "Координаты после поворота:" << std::endl;
+        std::cout << "Верхняя левая: (" << x1 << ", " << y1 << ")" << std::endl;
+        std::cout << "Верхняя правая: (" << x2 << ", " << y2 << ")" << std::endl;
+        std::cout << "Нижняя правая: (" << x3 << ", " << y3 << ")" << std::endl;
+        std::cout << "Нижняя левая: (" << x4 << ", " << y4 << ")" << std::endl;
+topLeft.setPosition(x1, y1);
+    }
+};
+class Square : public Rectangle {
+    public:
+        Square(double x, double y, double side)
+            : Rectangle(x, y, side, side) {}
+    
+        void draw() const override {
+            std::cout << "Рисуется квадрат с левой верхней точкой в (" 
+                      << topLeft.getX() << ", " << topLeft.getY()
+                      << "), сторона: " << width << std::endl;
+        }
+    
+        void erase() const override {
+            std::cout << "Стирается квадрат" << std::endl;
+        }
+    };
+    
+    class Parallelogram : public Shape {
+    protected:
+        BasicPoint topLeft;
+        double base, height, offset;
+    public:
+        Parallelogram(double x, double y, double b, double h, double o)
+            : topLeft(x, y), base(b), height(h), offset(o) {}
+    
+        void draw() const override {
+            std::cout << "Рисуется параллелограмм с левой верхней точкой в (" 
+                      << topLeft.getX() << ", " << topLeft.getY()
+                      << "), основание: " << base << ", высота: " << height
+                      << ", смещение: " << offset << std::endl;
+        }
+    
+        void erase() const override {
+            std::cout << "Стирается параллелограмм" << std::endl;
+        }
+    
+        void move(double dx, double dy) override {
+            topLeft.move(dx, dy);
+        }
+    
+        void rotate(double angle) override {
+            double radians = angle * PI / 180.0;
+            double centerX = topLeft.getX() + base / 2.0;
+            double centerY = topLeft.getY() + height / 2.0;
+            
+            double x1 = topLeft.getX();
+            double y1 = topLeft.getY();
+            double x2 = x1 + base;
+            double y2 = y1;
+            double x3 = x1 + offset;
+            double y3 = y1 + height;
+            double x4 = x2 + offset;
+            double y4 = y3;
+    
+            rotatePoint(x1, y1, centerX, centerY, radians);
+            rotatePoint(x2, y2, centerX, centerY, radians);
+            rotatePoint(x3, y3, centerX, centerY, radians);
+            rotatePoint(x4, y4, centerX, centerY, radians);
+    
+            std::cout << "Координаты после поворота:" << std::endl;
+            std::cout << "Верхняя левая: (" << x1 << ", " << y1 << ")" << std::endl;
+            std::cout << "Верхняя правая: (" << x2 << ", " << y2 << ")" << std::endl;
+            std::cout << "Нижняя левая: (" << x3 << ", " << y3 << ")" << std::endl;
+            std::cout << "Нижняя правая: (" << x4 << ", " << y4 << ")" << std::endl;
+    
+            topLeft.setPosition(x1, y1);
+        }
+    };
+    
+    class Rhombus : public Parallelogram {
+    public:
+        Rhombus(double x, double y, double d1, double d2)
+            : Parallelogram(x, y, d1, d2, 0) {}
+    
+        void draw() const override {
+            std::cout << "Рисуется ромб с левой верхней точкой в (" 
+                      << topLeft.getX() << ", " << topLeft.getY()
+                      << "), диагонали: " << base << " и " << height << std::endl;
+        }
+    
+        void erase() const override {
+            std::cout << "Стирается ромб" << std::endl;
+        }
+    };
+    
+    void demo(Shape* shape) {
+        shape->draw();
+        shape->move(5, 5);
+        shape->rotate(45);
+        shape->draw();
+        shape->erase();
+    }
+    
+    int main() {
+        Square sq(0, 0, 10);
+        Line ln(0, 0, 5, 5);
+        Rectangle rect(1, 1, 8, 4);
+        Parallelogram pg(2, 2, 6, 4, 3);
+        Rhombus rh(3, 3, 5, 7);
+    
+        std::cout << "Демонстрация: Квадрат" << std::endl;
+        demo(&sq);
+    
+        std::cout << "\nДемонстрация: Линия" << std::endl;
+        demo(&ln);
+    
+        std::cout << "\nДемонстрация: Прямоугольник" << std::endl;
+        demo(&rect);
+    
+        std::cout << "\nДемонстрация: Параллелограмм" << std::endl;
+        demo(&pg);
+    
+        std::cout << "\nДемонстрация: Ромб" << std::endl;
+        demo(&rh);
+    
+        return 0;
+    }
